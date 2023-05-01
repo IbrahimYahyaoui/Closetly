6d4672b2c3361ea9f29d1c9c21d45f75572feab4
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-05-01 01:43:23 +0100

    fix conflict
    conflict between tailwind and nextui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./Pages/AuthPage/context/AuthContext.jsx";
-
+import { NextUIProvider } from "@nextui-org/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <NextUIProvider disableBaseline="true">
+        <App />
+      </NextUIProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
