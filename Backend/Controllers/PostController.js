ec00b2507e38296fd7c7d9d7111ad2f88c5dca89
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-05-22 14:54:57 +0100

    --
    get post from backend

--- Backend/Controllers/PostController.js
+++ Backend/Controllers/PostController.js
@@ -1,4 +1,5 @@
 const Post = require("../Models/PostModel");
+const mongoose = require("mongoose");
 // add post
 const addPost = async (req, res) => {
   const { Description, Outfit, UserId } = req.body;
@@ -10,6 +11,35 @@ const addPost = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+// get post
+const getPost = async (req, res) => {
+  // get id from params
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send("Invalid post id");
+  }
+
+  try {
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).send(post);
+    } else {
+      res.status(404).send("Post not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+// get All post
+const getAllPosts = async (req, res) => {
+  try {
+    const Allposts = await Post.find({});
+    res.status(200).send(Allposts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 // delete post
 const DeletePost = (req, res) => {
   res.send("delete");
@@ -18,4 +48,6 @@ const DeletePost = (req, res) => {
 module.exports = {
   addPost,
   DeletePost,
+  getPost,
+  getAllPosts,
 };
--- Backend/Routes/PostRoutes.js
+++ Backend/Routes/PostRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { addPost, DeletePost } = require("../Controllers/PostController");
+const {
+  addPost,
+  DeletePost,
+  getPost,
+  getAllPosts,
+} = require("../Controllers/PostController");
 const router = express.Router();
 
 // add post
 router.post("/addPost", addPost);
+// get All post
+router.get("/getAllPosts", getAllPosts);
+// get post by id
+router.post("/getPost/:id", getPost);
+
 // delete post
 router.post("/deletePost", DeletePost);
 
